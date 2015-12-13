6288f05afc661f80bc5bec70c135b39348af2e54
Mike Flynn <mfflynn@gmail.com>
2015-12-12 16:36:54 -0800

    Added top-level categories.

--- statics/categories.js
+++ statics/categories.js
@@ -1,7 +1,67 @@
 module.exports = [
   {
+    name:        'Travel',
+    description: 'We take the best trips.',
+    slug:        '/travel',
+    items: [
+      {
+        description: '',
+        image: '',
+        owner: '',
+        distance: '',
+        utilization: '',
+        cost: ''
+      }
+    ]
+  },
+  {
+    name:        'Electronics',
+    description: 'We use these to instagram the other stuff we own.',
+    slug:        '/electronics',
+    items: [
+      {
+        description: '',
+        image: '',
+        owner: '',
+        distance: '',
+        utilization: '',
+        cost: ''
+      }
+    ]
+  },
+  {
+    name:        'Entertainment',
+    description: 'Things we look at when we get bored with our other stuff.',
+    slug:        '/entertainment',
+    items: [
+      {
+        description: '',
+        image: '',
+        owner: '',
+        distance: '',
+        utilization: '',
+        cost: ''
+      }
+    ]
+  },
+  {
+    name:        'Food',
+    description: "We're so full.",
+    slug:        '/food',
+    items: [
+      {
+        description: '',
+        image: '',
+        owner: '',
+        distance: '',
+        utilization: '',
+        cost: ''
+      }
+    ]
+  },
+  {
     name:        'Home & Gardening',
-    description: 'Home and Gardening tools',
+    description: 'We have really pretty homes and gardens.',
     slug:        '/home',
     items: [
       {
@@ -15,11 +75,48 @@ module.exports = [
     ]
   },
   {
-    name:        'Power Tools',
-    slug:        '/tools',
-    description: 'Power tools you can\'t use',
+    name:        'Shelter',
+    description: "It's like the perfect temperature in here right now.",
+    slug:        '/shelter',
     items: [
-
+      {
+        description: '',
+        image: '',
+        owner: '',
+        distance: '',
+        utilization: '',
+        cost: ''
+      }
+    ]
+  },
+  {
+    name:        'Companionship',
+    description: 'Love. So much love.',
+    slug:        '/companionship',
+    items: [
+      {
+        description: '',
+        image: '',
+        owner: '',
+        distance: '',
+        utilization: '',
+        cost: ''
+      }
+    ]
+  },
+  {
+    name:        'Help',
+    description: "Nothing is wrong with us right now. We're fine.",
+    slug:        '/help',
+    items: [
+      {
+        description: '',
+        image: '',
+        owner: '',
+        distance: '',
+        utilization: '',
+        cost: ''
+      }
     ]
   }
 ];
