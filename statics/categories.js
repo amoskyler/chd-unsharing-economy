ec84481c6176edea0bebbdbff3c2a0f76475756f
Mike Flynn <mfflynn@gmail.com>
2015-12-12 16:44:34 -0800

    Added array index to categories.

--- statics/categories.js
+++ statics/categories.js
@@ -1,5 +1,5 @@
 module.exports = [
-  {
+  travel: {
     name:        'Travel',
     description: 'We take the best trips.',
     slug:        '/travel',
@@ -14,7 +14,7 @@ module.exports = [
       }
     ]
   },
-  {
+  electronics: {
     name:        'Electronics',
     description: 'We use these to instagram the other stuff we own.',
     slug:        '/electronics',
@@ -29,7 +29,7 @@ module.exports = [
       }
     ]
   },
-  {
+  entertainment: {
     name:        'Entertainment',
     description: 'Things we look at when we get bored with our other stuff.',
     slug:        '/entertainment',
@@ -44,7 +44,7 @@ module.exports = [
       }
     ]
   },
-  {
+  food: {
     name:        'Food',
     description: "We're so full.",
     slug:        '/food',
@@ -59,7 +59,7 @@ module.exports = [
       }
     ]
   },
-  {
+  home: {
     name:        'Home & Gardening',
     description: 'We have really pretty homes and gardens.',
     slug:        '/home',
@@ -74,7 +74,7 @@ module.exports = [
       }
     ]
   },
-  {
+  shelter: {
     name:        'Shelter',
     description: "It's like the perfect temperature in here right now.",
     slug:        '/shelter',
@@ -89,22 +89,30 @@ module.exports = [
       }
     ]
   },
-  {
+  companionship: {
     name:        'Companionship',
     description: 'Love. So much love.',
     slug:        '/companionship',
     items: [
       {
-        description: '',
-        image: '',
-        owner: '',
-        distance: '',
-        utilization: '',
-        cost: ''
+        description: "I have the best dog. She's like a movie dog. Plays catch. Uses the toilet. Doesn't shed on my other expensive shit. I forgot to feed her for a while and she's still alive.",
+        image: 'http://i.imgur.com/GGvLcKN.jpg',
+        owner: 'Jerry',
+        distance: '2 mi',
+        utilization: '27%',
+        cost: '$1000'
+      },
+      {
+        description: "My family is perfect. Everyone has nice teeth and there aren't any bald or fat people. Haven't talked to them in months but they are just lovely. ",
+        image: 'http://i.imgur.com/w4P0eVB.jpg',
+        owner: 'Chris',
+        distance: '0.8 mi',
+        utilization: '4%',
+        cost: '$0'
       }
     ]
   },
-  {
+  help: {
     name:        'Help',
     description: "Nothing is wrong with us right now. We're fine.",
     slug:        '/help',
