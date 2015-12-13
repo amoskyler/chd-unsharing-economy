fb69c3fff211950c95ea887191aed0bfc24fe4db
Amos Kyler <amos.kyler@gmail.com>
2015-12-12 16:16:04 -0800

    gulp

--- api/get.js
+++ api/get.js
@@ -6,9 +6,22 @@ var config = require('../config');
 module.exports = function (app) {
   app.get('/', function(req, res){
 
-    var fn = Jade.compileFile('../templates/index');
+    var categories = [
+      {
+        name:        'Home & Gardening',
+        description: 'Home and Gardening tools',
+        link:        config.site_url + '/home'
+      },
+      {
+        name:        'Power Tools',
+        description: 'Power tools you can\'t use',
+        link:        config.site_url + '/tools'
+      }
+    ];
 
-    var html = fn();
+    var fn = Jade.compileFile('./templates/index.jade');
+
+    var html = fn({categories: categories});
 
     return res.send(html);
 
